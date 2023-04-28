9e0c31dde963f42cc3901a395a81d79a7b3b84ca
kohshi54 <kohshi54.yam@gmail.com>
2023-04-29 04:03:02 +0900

    update

--- ft_client.c
+++ ft_client.c
@@ -1,33 +1,24 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "LIBFT/libft.h"
-#include <signal.h>
 #include "FT_PRINTF/ft_printf.h"
-//  cc -o client ft_client.c LIBFT/ft_atoi.c LIBFT/ft_isdigit.c FT_PRINTF/ft_printf.c FT_PRINTF/ft_printf_utils.c FT_PRINTF/ft_convert_base.c
+#include <signal.h>
+//cc -o client ft_client.c LIBFT/ft_atoi.c LIBFT/ft_isdigit.c FT_PRINTF/ft_printf.c FT_PRINTF/ft_printf_utils.c FT_PRINTF/ft_convert_base.c LIBFT/ft_bzero.c
 
 void    send_char(pid_t pid, int c)
 {
-	unsigned char   byte;
-	size_t			i;
+	ssize_t	i;
 
-	i = 0;
-	byte = 1 << 7;
-	while (i < 8)
+	i = 7;
+	while (0 <= i)
 	{
-		if (c & byte)
-		{
-			// ft_printf("1");
+		if (c & (1 << i))
 			kill(pid, SIGUSR1);
-		}
-
 		else
-		{
-			// ft_printf("0");
 			kill(pid, SIGUSR2);
-		}
-		byte = byte >> 1;
-		i++;
-		usleep(100);
+		pause();
+		usleep(60);
+		i--;
 	}
 }
 
@@ -38,34 +29,43 @@ void    send_str(pid_t pid, char *str)
 		send_char(pid, *str);
 		str++;
 	}
+	send_char(pid, '\0');
 }
 
-void signal_handler(int signal, siginfo_t *info, void *context)
+void signal_handler(int signum, siginfo_t *info, void *context)
 {
-	ft_printf("ok\n");
-}
-
-void set_signal_action(struct sigaction *sa)
-{
-	ft_bzero(sa, sizeof(*sa));
-	(*sa).sa_flags = SA_SIGINFO;
-	(*sa).sa_sigaction = &signal_handler; // ポインタにするのかしないのか
-	sigaction(SIGUSR1, sa, NULL);
-	// sigaction(SIGUSR2, sa, NULL);
+	if (signum == SIGUSR2)
+	{
+		ft_printf("ok\n");
+		exit(EXIT_SUCCESS);
+	}
 }
 
 int main(int argc, char *argv[])
 {
-	pid_t   pid;
-	struct sigaction	sa;
+	struct sigaction	act;
+	pid_t				pid;
 
 	if (argc != 3)
 	{
 		ft_printf("wrong argc");
 		return (1);
 	}
-	set_signal_action(&sa);
+
+	sigemptyset(&act.sa_mask);
+	sigaddset(&act.sa_mask, SIGUSR1);
+	sigaddset(&act.sa_mask, SIGUSR2);
+
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = signal_handler;
+
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+
 	pid = ft_atoi(argv[1]);
 	send_str(pid, argv[2]);
+
+	pause();
+
 	return (0);
 }
--- ft_server.c
+++ ft_server.c
@@ -1,69 +1,56 @@
-#include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
 #include "FT_PRINTF/ft_printf.h"
 #include "LIBFT/libft.h"
-
-//cc -Wall -Wextra -Werror ft_server.c FT_PRINTF/ft_printf.c FT_PRINTF/ft_convert_base.c FT_PRINTF/ft_printf_utils.c
-
 #include <stdlib.h>
-#include <unistd.h>
+//cc -Wall -Wextra -Werror -o server ft_server.c FT_PRINTF/ft_printf.c FT_PRINTF/ft_convert_base.c FT_PRINTF/ft_printf_utils.c LIBFT/ft_bzero.c 
 
-// volatile sig_atomic_t flag = 0;
 volatile sig_atomic_t byte = 0;
 
-void signal_handler(int signal, siginfo_t *info, void *context)
+void signal_handler(int signum, siginfo_t *info, void *context)
 {
-	volatile static sig_atomic_t i;
-	char c;
+	volatile static sig_atomic_t i = 0;
+	volatile sig_atomic_t c;
 	
 	(void)context;
-	if (signal == SIGUSR1)
+	byte = byte << 1;
+	if (signum == SIGUSR1)
 		byte = byte | 1;
-	// ft_printf("byte: %d\n", byte);
-	if (i == 7)
+	if (++i == 8)
 	{
-		c = byte;
-		write(STDOUT_FILENO, &c, 1);
+		c = 0xff & byte;
+		if (c == '\0')
+		{
+			usleep(60);
+			kill(info->si_pid, SIGUSR2);
+		}
+		else
+			write(STDOUT_FILENO, (char *) &c, sizeof(char));
 		i = 0;
 		byte = 0;
-		// flag = 1;
-		// ft_printf("received from pid: %d\n", (int)info->si_pid);
-		kill(info->si_pid, SIGUSR1);
 	}
-	else
-	{
-		i++;
-		byte = byte << 1;
-	}
+	usleep(60);
+	kill(info->si_pid, SIGUSR1);
 }
 
-void set_signal_action(struct sigaction *sa)
+int	main(void)
 {
-	ft_bzero(sa, sizeof(*sa));
-	(*sa).sa_flags = SA_SIGINFO;
-	(*sa).sa_sigaction = &signal_handler; // ポインタにするのかしないのか
-	sigaction(SIGUSR1, sa, NULL);
-	sigaction(SIGUSR2, sa, NULL);
-}
-
-int main()
-{
-	struct sigaction	sa;
+	struct sigaction	act;
 
 	ft_printf("pid: %d\n", getpid());
-	set_signal_action(&sa);
+
+	sigemptyset(&act.sa_mask);
+	sigaddset(&act.sa_mask, SIGUSR1);
+	sigaddset(&act.sa_mask, SIGUSR2);
+
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = signal_handler;
+
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+
 	while (1)
-	{
-		// printf("Wasting your cycles: %d\n", getpid());
-		// if (flag)
-		// {
-			// ft_printf("\nreceived signal\n");
-			// flag = 0;
-		// }
-			
-		// sleep(1);
-		pause();
-	}
-	// pause();
+		(void)pause();
+
+	return (0);
 }
